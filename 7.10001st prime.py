fc5d69c4a50952d23d9dc29ecd8b4fc26396dbe7
Noophi <mourne.com@gmail.com>
2019-05-14 00:36:15 +0900

    duration: 0.75sec

--- 7.10001st prime.py
+++ 7.10001st prime.py
@@ -2,12 +2,15 @@ import prime, time
 s=time.time()
 p = prime
 
-l=[2]
-i=2
+
+l=[2,3]
+# 2를 제외하고 짝수는 소수가 아니므로, 3부터 +2씩 증가하며 판단하는게 빠름
+# 물론 isprime함수에서 가장 먼저 2로 나누어보지만, 그래도 애초에 안 나눠보는게 20% 정도 속도 빨라짐
+
+i=3
 while len(l)!=10001:
-    i+=1
-    if p.isprime(i)==True:
-        l.append(i)
+    i+=2
+    if p.isprime(i)==True: l.append(i)
 
 print(i)
 

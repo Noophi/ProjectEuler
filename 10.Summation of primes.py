4a5420c86855cd819cd58ebd4748300179abf387
Noophi <mourne.com@gmail.com>
2019-05-14 02:44:59 +0900

    duration: 20sec

--- 10.Summation of primes.py
+++ 10.Summation of primes.py
@@ -8,7 +8,7 @@ import prime, time
 isprime = prime.isprime
 s = time.time()
 
-#2는 애초에 넣어놓고, 3부터 +2씩하면서 소수를 찾는 계산이 30%정도 빠름.
+#2는 애초에 넣어놓고, 3부터 +2씩하면서 소수를 찾는 계산이 30%정도 빠름
 i=3
 sum=2
 while i<2000000:

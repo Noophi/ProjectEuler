aac1e4aad68c14470240b23175ea27c0bc3be833
Noophi <mourne.com@gmail.com>
2019-05-14 02:42:55 +0900

    10. Summation of primes

---
+++ 10.Summation of primes.py
@@ -0,0 +1,19 @@
+'''
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million.
+'''
+
+import prime, time
+
+isprime = prime.isprime
+s = time.time()
+
+#2는 애초에 넣어놓고, 3부터 +2씩하면서 소수를 찾는 계산이 30%정도 빠름.
+i=3
+sum=2
+while i<2000000:
+    if isprime(i)==True : sum+=i
+    i+=2
+
+print(sum)
+print(time.time()-s)

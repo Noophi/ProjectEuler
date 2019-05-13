a9b672a6f14086a9e92ff62cf49b9387133f4c7f
Noophi <mourne.com@gmail.com>
2019-05-14 03:45:30 +0900

    update

--- 10.Summation of primes.py
+++ 10.Summation of primes.py
@@ -5,14 +5,14 @@ Find the sum of all the primes below two million.
 
 import prime, time
 
-isprime = prime.isprime
+p = prime.isprime
 s = time.time()
 
 #2는 애초에 넣어놓고, 3부터 +2씩하면서 소수를 찾는 계산이 30%정도 빠름
 i=3
 sum=2
 while i<2000000:
-    if isprime(i)==True : sum+=i
+    if p(i)==True : sum+=i
     i+=2
 
 print(sum)
